2590679ea9fb19d1034c30c6123e3b59e413bd7c
Kabelo Ranthakoana <ranthyprinkay@gmail.com>
2025-07-04 14:12:07 +0200

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from pytz import timezone
+import pytz
+
+app = Flask(__name__)
+login = LoginManager(app)
+login.login_view = 'login'
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+@app.template_filter('localtime')
+def localtime_filter(utc_dt):
+    if utc_dt is None:
+        return "Never"
+    sa_tz = timezone('Africa/Johannesburg')
+    if utc_dt.tzinfo is None:
+        utc_dt = pytz.utc.localize(utc_dt)
+    return utc_dt.astimezone(sa_tz).strftime('%Y-%m-%d %H:%M:%S')
+
+
+
+
+from app import routes, models
+
+
---
+++ app/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, data_required, Email, EqualTo, Length
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo("password", message="Passwords must match")])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError("Please use a different Username.")
+        
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError("Please use a different Email address.")
+        
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    
